8cf4dabb06191dfa2f0599ace545c91569d78815
noah <noah@noah-X510UAR>
2021-02-12 10:14:43 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Accounting
+
+Accounting System
+
+#### License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in accounting/__init__.py
+from accounting import __version__ as version
+
+setup(
+	name='accounting',
+	version=version,
+	description='Accounting System',
+	author='Noah Jacob',
+	author_email='noahj@something.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
