67528bf7762b62029def2a9728d7c55c34b0adbd
= <noahjacobkurian@gmail.com>
2021-03-10 18:00:25 +0530

    Test cases- Purchase Order

--- accounting/accounting/doctype/purchase_order/purchase_order.py
+++ accounting/accounting/doctype/purchase_order/purchase_order.py
@@ -8,13 +8,26 @@ from frappe.model.document import Document
 from frappe.utils import flt
 class PurchaseOrder(Document):
 	def validate(self):
-		self.total_amount,self.total_quantity = 0,0
-		if not self.items:
-			frappe.throw("Add some Items before saving")
-		for d in self.items:
-			self.total_quantity = flt(self.total_quantity) + flt(d.item_quantity)
-			self.total_amount = flt(self.total_amount) + flt(d.amount)
+		calc_totals(self)
+		validate_quantity(self)
 
+
+		
 	
+def calc_totals(self):
+	self.total_amount,self.total_quantity = 0,0
+	if not self.items:
+		frappe.throw("Add some Items before saving")
+	for d in self.items:
+		d.amount = d.item_quantity * d.item_rate
+	for d in self.items:
+		self.total_quantity = flt(self.total_quantity) + flt(d.item_quantity)
+		self.total_amount = flt(self.total_amount) + flt(d.amount)
+
+def validate_quantity(self):
+	for d in self.items:
+		if d.item_quantity < 0 or d.item_quantity == 0:
+			frappe.throw("Item quantity is invalid")
+
 		
 			
--- accounting/accounting/doctype/purchase_order/test_purchase_order.py
+++ accounting/accounting/doctype/purchase_order/test_purchase_order.py
@@ -3,8 +3,73 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
+from frappe.utils import nowdate,flt
 import unittest
 
 class TestPurchaseOrder(unittest.TestCase):
-	pass
+
+	def test_new_purchase_order(self):
+		p_order = create_purchase_order('Gamestop','Nintendo Switch',2,True,False)
+		self.assertTrue(check_created_order(p_order.name),p_order.name)
+		total_amount, total_quantity = 0,0
+		for d in p_order.items:
+			total_quantity+=flt(d.item_quantity)
+			total_amount +=flt(d.amount)
+		self.assertEqual(p_order.total_amount,total_amount) 
+		self.assertEqual(p_order.total_quantity,total_quantity)
+		delete_orders(p_order.name)
+
+	def test_validations(self):
+		p_order = create_purchase_order('Gamestop','Nintendo Switch',-2,False,False)
+		self.assertRaises(frappe.exceptions.ValidationError,p_order.insert)
+		delete_orders(p_order.name)
+		p_order = create_purchase_order('Gamestop','Nintendo Switch',0,False,False)
+		self.assertRaises(frappe.exceptions.ValidationError,p_order.insert)
+		delete_orders(p_order.name)
+		p_order = create_purchase_order('Gamestop',None,None,False,False)
+		self.assertRaises(frappe.exceptions.ValidationError,p_order.insert)
+		delete_orders(p_order.name)
+		
+		
+
+
+def create_purchase_order(supplier,item_name,qty,save =True,submit = False ):
+	p_order = frappe.new_doc('Purchase Order')
+	p_order.posting_date = nowdate()
+	p_order.supplier = "Gamestop"
+	p_order.company = "Test"
+	if item_name and qty:
+		p_order.set("items",[
+			{
+				"item_name":item_name,
+				"item_quantity":qty
+			},
+			{
+				"item_name":item_name,
+				"item_quantity":qty
+			},
+		])
+
+	if save or submit:
+		p_order.save()
+		if submit:
+			p_order.submit()
+	return p_order
+
+
+
+def check_created_order(voucher_no):
+	order =  frappe.db.sql("""SELECT
+								*
+							FROM
+								`tabPurchase Order`
+							WHERE 
+								name = %s""",voucher_no,as_dict = 1)
+
+	return order
+def delete_orders(voucher_no):
+	frappe.delete_doc("Purchase Order",voucher_no)			
+
+	
+
