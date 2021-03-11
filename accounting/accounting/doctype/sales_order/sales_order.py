5e32ff98d7f63da4c3a44faaddde9e92c8037666
= <noahjacobkurian@gmail.com>
2021-03-11 10:27:38 +0530

    Test Cases - Sales Order

--- accounting/accounting/doctype/sales_order/sales_order.py
+++ accounting/accounting/doctype/sales_order/sales_order.py
@@ -10,12 +10,25 @@ from frappe.utils import flt
 
 class SalesOrder(Document):
 	def validate(self):
-		self.total_amount, self.total_quantity, self.total_cost_price = 0, 0, 0
-		if not self.items:
-			frappe.throw("Add some Items before saving")
-		for d in self.items:
-			cost_price = flt(d.item_quantity) * flt(d.standard_rate)
-			self.total_cost_price = self.total_cost_price + cost_price
-			self.total_quantity = flt(self.total_quantity) + flt(d.item_quantity)
-			self.total_amount = flt(self.total_amount) + flt(d.amount)
-			
+		calc_totals(self)
+		validate_quantity(self)
+		
+
+
+def calc_totals(self):
+	self.total_amount, self.total_quantity, self.total_cost_price = 0, 0, 0
+	if not self.items:
+		frappe.throw("Add some Items before saving")
+	for d in self.items:
+		d.amount = d.item_quantity * d.item_rate
+	for d in self.items:
+		cost_price = flt(d.item_quantity) * flt(d.standard_rate)
+		self.total_cost_price = self.total_cost_price + cost_price
+		self.total_quantity = flt(self.total_quantity) + flt(d.item_quantity)
+		self.total_amount = flt(self.total_amount) + flt(d.amount)
+
+
+def validate_quantity(self):
+	for d in self.items:
+		if d.item_quantity < 0 or d.item_quantity == 0:
+			frappe.throw("Item quantity is invalid")
--- accounting/accounting/doctype/sales_order/test_sales_order.py
+++ accounting/accounting/doctype/sales_order/test_sales_order.py
@@ -3,8 +3,67 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
+from frappe.utils import flt,nowdate
 import unittest
 
 class TestSalesOrder(unittest.TestCase):
-	pass
+	def test_new_sales_order_totals(self):
+		s_order = create_sales_order('Noah','Nintendo Switch',1,30000,True,False)
+		self.assertTrue(check_created_order(s_order.name))
+		self.assertEqual(s_order.total_cost_price,50000)
+		self.assertEqual(s_order.total_amount,60000)
+		self.assertEqual(s_order.total_quantity,2)
+		delete_orders(s_order.name)
+
+
+
+	def test_validations(self):
+			s_order = create_sales_order('Noah','Nintendo Switch',-1,30000,False,False)
+			self.assertRaises(frappe.exceptions.ValidationError,s_order.insert)
+			delete_orders(s_order.name)
+			s_order = create_sales_order('Noah','Nintendo Switch',0,30000,False,False)
+			self.assertRaises(frappe.exceptions.ValidationError,s_order.insert)
+			delete_orders(s_order.name)
+			s_order = create_sales_order('Noah',None,None,None,False,False)
+			self.assertRaises(frappe.exceptions.ValidationError,s_order.insert)
+			delete_orders(s_order.name)
+
+
+def create_sales_order(customer,item_name,qty,rate,save =True,submit = False ):
+	s_order = frappe.new_doc('Sales Order')
+	s_order.posting_date = nowdate()
+	s_order.customer = customer
+	s_order.company = "Test"
+
+	if item_name and qty:
+		s_order.set("items",[
+			{
+				"item_name":item_name,
+				"item_quantity":qty,
+				"item_rate":rate
+			},
+			{
+				"item_name":item_name,
+				"item_quantity":qty,
+				"item_rate":rate
+			},
+		])
+
+	if save or submit:
+		s_order.save()
+		if submit:
+			s_order.submit()
+	return s_order
+
+def check_created_order(voucher_no):
+	order =  frappe.db.sql("""SELECT
+								*
+							FROM
+								`tabSales Order`
+							WHERE 
+								name = %s""",voucher_no,as_dict = 1)
+
+	return order
+def delete_orders(voucher_no):
+	frappe.delete_doc("Sales Order",voucher_no)
